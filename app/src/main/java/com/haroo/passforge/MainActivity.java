27539a90305b1d5c21c3e90a438d2ce553921996
M-Jey <mjeyict3@gmail.com>
2024-11-10 23:14:24 +0800

    Updated functions , Added Icon

--- app/src/main/java/com/haroo/passforge/MainActivity.java
+++ app/src/main/java/com/haroo/passforge/MainActivity.java
@@ -4,21 +4,23 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 import java.util.Random;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    private Character[] symbols = {'.', '-', '+', '#', '@'};
     private int passLen = 12;
     private String password = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
@@ -30,16 +32,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public String getSaltString() {
+        CheckBox symbolsBox = findViewById(R.id.symbolsBox);
+        CheckBox numbersBox = findViewById(R.id.numbersBox);
         String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String SALTCHARSsym = "ABCDEFGHIJKLMNOPQRSTUVWXYZ-+=_':/.?";
+        String SALTCHARSnum = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        String SALTCHARSmix = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-+=_':/.?";
         StringBuilder salt = new StringBuilder();
         Random rnd = new Random();
-        while (salt.length() < passLen) { // length of the random string.
-            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
-            salt.append(SALTCHARS.charAt(index));
-        }
-        String saltStr = salt.toString();
-        return saltStr;
+        String saltStr = "";
+        int index;
 
+        while (salt.length() < passLen) { // length of the random string.
+            if(symbolsBox.isChecked()) {
+                index = (int) (rnd.nextFloat() * SALTCHARSsym.length());
+                salt.append(SALTCHARSsym.charAt(index));
+            } else if(numbersBox.isChecked()) {
+                index = (int) (rnd.nextFloat() * SALTCHARSnum.length());
+                salt.append(SALTCHARSnum.charAt(index));
+            } else if (symbolsBox.isChecked() && numbersBox.isChecked()) {
+                index = (int) (rnd.nextFloat() * SALTCHARSmix.length());
+                salt.append(SALTCHARSmix.charAt(index));
+            }else{
+                index = (int) (rnd.nextFloat() * SALTCHARS.length());
+                salt.append(SALTCHARS.charAt(index));
+            }
+            saltStr = salt.toString();
+        }
+
+        return saltStr;
     }
 
     public void generatePassword(View view) {
